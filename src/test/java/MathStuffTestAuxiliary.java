2fb60090ea6c2ad3d642256557c1e5bd96dedb19
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-11-24 11:06:16 +0100

    initial commit

---
+++ src/test/java/MathStuffTestAuxiliary.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+/**
+ * Test cases for auxiliary methods in {@code MathStuff}.
+ *
+<!--//# BEGIN TODO: Name, student ID, and date-->
+<p><b>Tygo van den Hurk, 1705709, 24-nov-2023</b></p>
+<!--//# END TODO-->
+ */
+public class MathStuffTestAuxiliary {
+
+    //# BEGIN TODO: Test cases for auxiliary functions
+
+    //# END TODO
+
+}
